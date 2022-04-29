e84419ca23048995df66f86eb59e6442592621d8
Kyle-hough <kyle.hough05@gmailcom>
2022-04-29 11:39:13 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function change(element){
+    var name = document.querySelector (".card-body h1");
+    name.innerText = "John Doe";
+    console.log (name);
+}
+
+function removeTod(Tod){
+    var Tod = document.querySelector (".Todd")
+    Tod.remove();
+}
+
+function removePhil(Phil){
+    var Phil = document.querySelector (".Phil")
+    Phil.remove();
+}
